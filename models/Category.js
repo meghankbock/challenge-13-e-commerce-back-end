bb60e804acafbcd99c25c82bb996142bf5a1c9d2
Meghan Bock <meghan.bock@gmail.com>
2021-08-15 23:22:50 -0500

    setup category model

--- models/Category.js
+++ models/Category.js
@@ -1,24 +1,25 @@
 const { Model, DataTypes } = require('sequelize');
+
 const sequelize = require('../config/connection.js');
 
 class Category extends Model {}
 
 Category.init(
-  {
-    id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true
-    },
-    category_name: {
-      type: DataTypes.STRING,
+    {
+      id: {
+      type: DataTypes.INTEGER,
       allowNull: false,
-      validate: {
-        len: [1]
+      primaryKey: true,
+      autoIncrement: true
+      },
+      category_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1]
+        }
       }
-    }
-  },
+    },
   {
     sequelize,
     timestamps: false,
